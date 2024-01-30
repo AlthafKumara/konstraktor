a938f4cd347c717dcdf936d7aa74a21662658ffc
Amartyaco Althaf Kumara <156567167+AlthafKumara@users.noreply.github.com>
2024-01-30 13:43:48 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+package konstraktor;
+
+
+public class main {
+    public static void main(String[] args) {
+        User petani = new User();
+        
+        petani.setUsername("Althaf");
+        petani.setPassword("Althaf123");
+        
+        System.out.println("Username : "+ petani.getUsername());
+        System.out.println("Password : "+ petani.getPassword());
+        
+        
+        
+    }
+    
+}
